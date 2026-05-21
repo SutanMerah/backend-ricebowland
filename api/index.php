4ac2b5c1dab44c7ef38d879035c09c6e7422c0a5
Hafizhan Azka <hafizhanazka92@gmail.com>
2026-05-22 00:17:02 +0700

    Debug

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Pastikan Vercel melacak autoloader Vendor secara absolut
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require __DIR__ . '/../vendor/autoload.php';
-}
-
-// Jalankan aplikasi Laravel bawaan public
-require __DIR__ . '/../public/index.php';
--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
-
-// Trik agar Laravel mengenali base path di Vercel
-$_ENV['APP_PATH'] = __DIR__;
-
-// Panggil framework yang sudah terinstal di vendor
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-
-// Jalankan kernel dan kirim response
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+echo "PHP sudah jalan di Vercel!";
+exit;
