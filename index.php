69b4a555907f397c51b5c87e5c1c424ca039357b
Hafizhan Azka <hafizhanazka92@gmail.com>
2026-05-22 00:24:25 +0700

    Debug 3

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "PHP sudah jalan di Vercel!";
-exit;
+
+// Tentukan lokasi base path Laravel
+define('LARAVEL_START', microtime(true));
+
+// Load Composer Autoloader
+require __DIR__ . '/vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+// Jalankan Kernel HTTP
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+$kernel->terminate($request, $response);
